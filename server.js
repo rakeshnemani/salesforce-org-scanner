60a17add13a03ca80218a2e444f017c38262d84b
Rakesh Nemani <rakeshnemani@gmail.com>
2025-06-14 12:21:33 -0400

    saving session on redis cloud

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const expressSession = require("express-session");
 const RedisStore = require("connect-redis").default;
-const { createClient } = require("redis");
+const createClient = require("redis");
 
 const metadataRoutes = require('./app/routes/metadataRoutes');
 const authConfig = require('./app/config/auth');
